68daca47081549ea9deca8a630d14922a9743100
Ion Stici <ionsticidev@gmail.com>
2024-07-10 14:30:23 +0300

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,21 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+
+import GlobalStyles from "./styles/GlobalStyles";
+
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+
 export default function App() {
-  return null;
+  return (
+    <>
+      <GlobalStyles />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
