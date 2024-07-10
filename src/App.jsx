b20ddca1d6d69300145cebb45b06c08b5f05a441
Ion Stici <ionsticidev@gmail.com>
2024-07-10 15:49:05 +0300

    Implement login

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 
 import DarkModeProvider from "./contexts/DarkModeContext";
 
@@ -7,17 +10,46 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { staleTime: 0 } },
+});
+
 export default function App() {
   return (
     <DarkModeProvider>
-      <GlobalStyles />
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
 
-      <BrowserRouter>
-        <Routes>
-          <Route path="login" element={<Login />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
+        <GlobalStyles />
+
+        <BrowserRouter>
+          <Routes>
+            <Route path="login" element={<Login />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+
+        <Toaster
+          position="top-center"
+          gutter={12}
+          containerStyle={{ margin: "8px" }}
+          toastOptions={{
+            success: {
+              duration: 3000,
+            },
+            error: {
+              duration: 5000,
+            },
+            style: {
+              fontSize: "16px",
+              maxWidth: "500px",
+              padding: "16px 24px",
+              backgroundColor: "var(--color-grey-0)",
+              color: "var(--color-grey-700)",
+            },
+          }}
+        />
+      </QueryClientProvider>
     </DarkModeProvider>
   );
 }
--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -2,13 +2,58 @@ import Form from "../../ui/Form";
 import Button from "../../ui/Button";
 import Input from "../../ui/Input";
 import FormRowVertical from "../../ui/FormRowVertical";
+import SpinnerMini from "../../ui/SpinnerMini";
 
 import { useState } from "react";
+import { useLogin } from "./useLogin";
 
 export default function LoginForm() {
+  const [email, setEmail] = useState("guest@email.com");
+  const [password, setPassword] = useState("12345678");
+
+  const { login, isPending } = useLogin();
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (!email || !password) return;
+    login(
+      { email, password },
+      {
+        onSettled: () => {
+          setEmail("");
+          setPassword("");
+        },
+      }
+    );
   };
 
-  return <Form onSubmit={handleSubmit}></Form>;
+  return (
+    <Form onSubmit={handleSubmit}>
+      <FormRowVertical label="Email address">
+        <Input
+          type="email"
+          id="email"
+          autoComplete="username"
+          value={email}
+          onChange={({ target }) => setEmail(target.value)}
+          disabled={isPending}
+        />
+      </FormRowVertical>
+      <FormRowVertical label="Password">
+        <Input
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+          disabled={isPending}
+        />
+      </FormRowVertical>
+      <FormRowVertical>
+        <Button size="large" disabled={isPending}>
+          {!isPending ? "Log in" : <SpinnerMini />}
+        </Button>
+      </FormRowVertical>
+    </Form>
+  );
 }
